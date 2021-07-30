d030bd739298ee491e70ab9989b7ac689927f021
maru8605 <maru8605@hotmail.com>
2021-07-29 21:42:39 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from '@emotion/styled'
+
+
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background-size: 300px;
+  border: 2px solid black;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  font-size: 2rem;
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+
+`
+
+function consultarApi(){
+  console.log('consultando api...')
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor>
+      <Boton
+        onClick={consultarApi}
+      >
+        Obtener Frase
+      </Boton>
+    </Contenedor>
   );
 }
 
